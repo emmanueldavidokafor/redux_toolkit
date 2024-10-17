5a8b91174cec8e395bd5b09ba49987119948cc77
EmmaOkafor <okaforifebuche26@gmail.com>
2024-10-17 09:50:09 +0100

    Handler reducers by the aid of redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-// import { connect } from 'react-redux';
+import { counterActions } from '../store';
 
 import classes from './Counter.module.css';
 // import { Component } from 'react';
@@ -10,18 +10,18 @@ const Counter = () => {
 	const showCounter = useSelector((state) => state.showCounter);
 
 	const incrementHandler = () => {
-		dispatch({ type: 'increment' });
+		dispatch(counterActions.increment());
 	};
 
 	const increaseHandler = () => {
-		dispatch({ type: 'increase', amount: 5 });
+		dispatch(counterActions.increase(10));
 	};
 	const decrementHandler = () => {
-		dispatch({ type: 'decrement' });
+		dispatch(counterActions.decrement());
 	};
 
 	const toggleCounterHandler = () => {
-		dispatch({ type: 'toggle' });
+		dispatch(counterActions.toggleCounter());
 	};
 
 	return (
